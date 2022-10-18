6d11a58bf5b88a064f08ee7601582f3ea82b31fc
TsutsuiKazushi <k.tsutsui6@gmail.com>
2022-10-18 10:26:47 +0900

    update

--- c1ae/run.sh
+++ c1ae/run.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-python -u c1ae.py 4.2
+
+option=aaa
+
+help_message=$(cat << EOF
+Usage: $0 [Options] <required>
+
+Options:
+    --option: indiv or share  (default=${option})
+    --required: 4.2, 3.6, 3.0, 2.4, or 1.8
+EOF
+)
+. utils/parse_options.sh
+
+# Variables that must be specified are defined after parse_options.sh
+required=$1
+
+if [ $# -ne 1 ]; then
+    echo "${help_message}"
+    exit 1;
+fi
+
+set -euo pipefail
+
+echo "option = ${option}"
+echo "required = ${required}"
+
+python -u c1ae.py ${required}
